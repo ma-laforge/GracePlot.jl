ac33cfc3f5bb811f7839548639aef09ea2ff9cb7
MA Laforge <ma.laforge.49@gmail.com>
2016-06-18 15:40:28 -0400

    runtests.jl now just tests "using".

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,10 @@
 #Test code
 #-------------------------------------------------------------------------------
 
-#No real test code yet... just run demos:
-for i in 1:5
-	include("../sample/demo$i.jl")
-end
+using GracePlot
+
+warn("No real test code yet... just ensuring that \"using\" works.")
+
+info("Run sample/runsamples.jl for sample usage.")
 
 :Test_Complete
